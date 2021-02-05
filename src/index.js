1140bac94184a98d5a676469ed9fb8f61ac2fe1b
Ndiaye <elhadjiamadoundiaye@gmail.com>
2021-02-05 09:47:43 +0000

    Redux overviews (setup without react in deep)

--- src/index.js
+++ src/index.js
@@ -3,6 +3,45 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+
+//STORE *** - Globalized state - All the data or state
+
+
+//ACTION
+const increment = () => {
+  return {
+    type : 'INCREMENT'
+  }
+}
+
+const decrement = () => {
+  return {
+    type: 'DECREMENT'
+    //Or name: 'DECREMENT'
+  }
+}
+
+
+//REDUCER
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+  }
+};
+//Now we can add this to the store
+let store = createStore(counter);
+//Display it in the console
+store.subscribe( ()=> console.log(store.getState()))
+
+//DISPATCH --- to execute action
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(increment());
 
 ReactDOM.render(
   <React.StrictMode>
